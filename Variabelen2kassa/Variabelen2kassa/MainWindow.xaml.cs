3e2493a380b77a11150d2c5596eafdaf59ced70c
12402996 <12402996@student.pxl.be>
2024-10-10 10:07:37 +0200

    Add files via upload

---
+++ Variabelen2kassa/Variabelen2kassa/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Variabelen2kassa
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void VATNumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (VATNumberTextBox.Text.Length <= 10)
+            {
+                double VATNumber;
+                bool isValid = double.TryParse(VATNumberTextBox.Text, out VATNumber);
+                if (isValid)
+                { }
+                else {
+            }
+
+        }
+    }
+}
